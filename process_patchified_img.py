9e0623fc6b7b2c23d85c2815d0c605ca76f88050
Sabrine Krichen <s.krichen@instadeep.com>
2023-10-15 22:08:13 +0100

    add requirements file

--- process_patchified_img.py
+++ process_patchified_img.py
@@ -12,18 +12,18 @@ input_image_path = sys.argv[1]
 # Load the grascale image
 grayscale_image = cv2.imread(input_image_path, cv2.IMREAD_GRAYSCALE)
 img = cv2.imread(input_image_path)
-patch_size = 5
+PATCH_SIZE = 5
 
 # Patchify the image
-patches = patchify(grayscale_image, (patch_size, patch_size), step=patch_size)
+patches = patchify(grayscale_image, (PATCH_SIZE, PATCH_SIZE), step=PATCH_SIZE)
 
 
 # Calculate the brightness.
 brightness = get_patches_brightness(patches=patches)
-num_top_patches = 4
+NUM_TOP_PATCHES = 4
 
-top_brightness_indices = get_top_bright_indices(brightness, num_top_patches)
-top_bright_centers = get_top_bright_centers(top_brightness_indices, patch_size)
+top_brightness_indices = get_top_bright_indices(brightness, NUM_TOP_PATCHES)
+top_bright_centers = get_top_bright_centers(top_brightness_indices, PATCH_SIZE)
 top_bright_centers = sorted(top_bright_centers, key=lambda x: (x[0], x[1]))
 vertices = np.array(top_bright_centers)
 quadrilateral_area = calculate_quadrilateral_area(vertices)
@@ -32,9 +32,8 @@ cv2.line(img, top_bright_centers[0], top_bright_centers[1], RED, 2)
 cv2.line(img, top_bright_centers[0], top_bright_centers[2], RED, 2)
 cv2.line(img, top_bright_centers[1], top_bright_centers[3], RED, 2)
 cv2.line(img, top_bright_centers[2], top_bright_centers[3], RED, 2)
-# Filename
-filename = "updated_image.png"
 
-# Using cv2.imwrite() method
+FILENAME = "updated_image.png"
+
 # Saving the image
-cv2.imwrite(filename, img)
+cv2.imwrite(FILENAME, img)
