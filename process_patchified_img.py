e2d26b0a5e129525671d39b0685ff9eb370e4273
Sabrine Krichen <s.krichen@instadeep.com>
2023-10-15 19:05:58 +0100

    feat: add the main processing file

--- process_patchified_img.py
+++ process_patchified_img.py
@@ -1,8 +1,8 @@
 """Python file processing the patchified image."""
 import sys
+
 import cv2
 import numpy as np
-
 from patchify import patchify
 
 from patching_utils import *
@@ -10,12 +10,12 @@ from patching_utils import *
 input_image_path = sys.argv[1]
 
 # Load the grascale image
-grayscale_image = cv2.imread(input_image_path,cv2.IMREAD_GRAYSCALE)
+grayscale_image = cv2.imread(input_image_path, cv2.IMREAD_GRAYSCALE)
 img = cv2.imread(input_image_path)
 patch_size = 5
 
 # Patchify the image
-patches = patchify(grayscale_image, (patch_size,patch_size), step= patch_size) 
+patches = patchify(grayscale_image, (patch_size, patch_size), step=patch_size)
 
 
 # Calculate the brightness.
@@ -28,15 +28,13 @@ top_bright_centers = sorted(top_bright_centers, key=lambda x: (x[0], x[1]))
 vertices = np.array(top_bright_centers)
 quadrilateral_area = calculate_quadrilateral_area(vertices)
 RED = (0, 0, 255)
-cv2.line(img, top_bright_centers[0], top_bright_centers[1], RED, 2) 
-cv2.line(img, top_bright_centers[0], top_bright_centers[2], RED, 2) 
-cv2.line(img, top_bright_centers[1], top_bright_centers[3], RED, 2) 
-cv2.line(img, top_bright_centers[2], top_bright_centers[3], RED, 2) 
-# Filename 
-filename = 'updated_image.png'
-  
-# Using cv2.imwrite() method 
-# Saving the image 
+cv2.line(img, top_bright_centers[0], top_bright_centers[1], RED, 2)
+cv2.line(img, top_bright_centers[0], top_bright_centers[2], RED, 2)
+cv2.line(img, top_bright_centers[1], top_bright_centers[3], RED, 2)
+cv2.line(img, top_bright_centers[2], top_bright_centers[3], RED, 2)
+# Filename
+filename = "updated_image.png"
+
+# Using cv2.imwrite() method
+# Saving the image
 cv2.imwrite(filename, img)
-
-
